8beb7b64cea71b6667475d6780fa9c16e36babdb
Martin Ueding <dev@martin-ueding.de>
2012-02-12 21:11:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    author = "Martin Ueding",
+    author_email = "dev@martin-ueding.de",
+    name = "backup_scripts",
+    scripts = ["backup-status"],
+    version = "1.0",
+)
