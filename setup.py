b9b0159bba77eabd2925d858db83ef500ed2397a
Martin Ueding <dev@martin-ueding.de>
2014-07-19 23:59:47 +0200

    Improve style of setup.py

--- setup.py
+++ setup.py
@@ -8,28 +8,28 @@ from setuptools import setup, find_packages
 __docformat__ = "restructuredtext en"
 
 setup(
-    author = "Martin Ueding",
-    author_email = "dev@martin-ueding.de",
-    description = "Collection of backup scripts",
-    license = "GPL2",
+    author="Martin Ueding",
+    author_email="dev@martin-ueding.de",
+    description="Collection of backup scripts",
+    license="GPL2",
     classifiers=[
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python",
     ],
-    name = "backup-scripts",
-    packages = find_packages(),
-    entry_points = {
+    name="backup-scripts",
+    packages=find_packages(),
+    entry_points={
         'console_scripts': [
-            'android-sync = backupscripts.android:main',
-            'backup-external = backupscripts.external:main',
-            'backup-status = backupscripts.status:main',
+            'android-sync=backupscripts.android:main',
+            'backup-external=backupscripts.external:main',
+            'backup-status=backupscripts.status:main',
         ],
     },
     install_requires=[
         'termcolor',
     ],
-    url = "https://github.com/martin-ueding/backup-scripts",
+    url="https://github.com/martin-ueding/backup-scripts",
     download_url="http://martin-ueding.de/download/backup-scripts/",
-    version = "1.19.2",
+    version="2.0",
 )
