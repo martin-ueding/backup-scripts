d930e6df6e6b8a87d5ef63c321774419963681e0
Martin Ueding <dev@martin-ueding.de>
2014-07-19 18:41:41 +0200

    Work on better configuration

--- backupscripts/external.py
+++ backupscripts/external.py
@@ -7,6 +7,7 @@
 Traverses through the external drives and updates the backup on them.
 """
 
+import argparse
 import configparser
 import os
 import re
@@ -18,7 +19,7 @@ import backupscripts.status
 
 __docformat__ = "restructuredtext en"
 
-def backup_data(key, name, config):
+def backup_data(key, name, config, dry):
     """
     Creates a backup to ``target``.
     """
@@ -26,32 +27,44 @@ def backup_data(key, name, config):
     for exclude in config[key]['exclude'].split():
         excludes += config['exclude'][exclude].split(':')
 
-    source = os.path.expanduser("~/")
-
-    termcolor.cprint("Backup {}".format(name), attrs=['bold'])
-
     exclude_arg = []
     for exclude in excludes:
         exclude_arg.append('--exclude='+exclude)
 
+    sources = []
+    for include in config[key]['include'].split():
+        sources += config['include'][include].split(':')
 
-    if 'host' in config[key]:
-        dest = config[key]['host'] + ':' + config[key]['path']
-    else:
-        dest = config[key]['path']
-        if not os.path.isdir(dest):
+    termcolor.cprint("Backup {}".format(name), attrs=['bold'])
+
+    for source in sources:
+        termcolor.cprint('Handling', 'cyan')
+        rel_dest = os.path.dirname(source)
+        abs_dest_path = os.path.join(config[key]['path'], rel_dest)
+
+        if 'host' in config[key]:
+            dest = config[key]['host'] + ':' + abs_dest_path
+        else:
+            dest = abs_dest_path
+            if not os.path.isdir(dest):
+                return
+
+        command = ["rsync", "-avhE", "--delete", "--delete-excluded"]
+        if dry:
+            command.append('-n')
+        command += exclude_arg + ["--", source, dest]
+
+        print(command)
+
+        try:
+            subprocess.check_call(command)
+        except subprocess.CalledProcessError as e:
+            print(e)
             return
 
-    command = ["rsync", "-avhE", "--delete", "--delete-excluded"] + exclude_arg + ["--", source, dest]
+    backupscripts.status.update(name, 'to')
 
-    try:
-        subprocess.check_call(command)
-    except subprocess.CalledProcessError as e:
-        print(e)
-    else:
-        backupscripts.status.update(name, 'to')
-
-def backup_info(key, name, config):
+def backup_info(key, name, config, dry):
     target = config[key]['path']
 
     termcolor.cprint("Info {}".format(name), attrs=['bold'])
@@ -59,9 +72,12 @@ def backup_info(key, name, config):
     if not os.path.isdir(target):
         return
 
-    infofiles = [os.path.expanduser(path) for path in config['infofiles']['paths']]
+    infofiles = [os.path.expanduser(path) for path in config['infofiles']['paths'].split(':')]
 
-    command = ["rsync", "-avhE", "--delete", "--"] + infofiles + [target]
+    command = ["rsync", "-avhE", "--delete"]
+    if dry:
+        command.append('-n')
+    command += ["--"] + infofiles + [target]
 
     try:
         subprocess.check_call(command)
@@ -71,6 +87,13 @@ def backup_info(key, name, config):
         backupscripts.status.update(name, 'to')
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('names', nargs='*')
+    parser.add_argument('-n', dest='dry', action='store_true')
+    options = parser.parse_args()
+
+    os.chdir(os.path.expanduser('~'))
+
     config = configparser.ConfigParser()
     config.read(os.path.expanduser('~/.config/backup-scripts/backup-external.ini'))
 
@@ -80,14 +103,18 @@ def main():
     info_pattern = re.compile(r'Info (.*)')
     data_pattern = re.compile(r'Target (.*)')
 
+
     for key in config.sections():
         info_matcher = info_pattern.match(key)
         if info_matcher:
-            backup_info(key, info_matcher.group(1), config)
+            backup_info(key, info_matcher.group(1), config, options.dry)
 
+    for key in config.sections():
         data_matcher = data_pattern.match(key)
         if data_matcher:
-            backup_data(key, data_matcher.group(1), config)
+            name = data_matcher.group(1)
+            if len(options.names) == 0 or name in options.names:
+                backup_data(key, name, config, options.dry)
 
 if __name__ == "__main__":
     main()
