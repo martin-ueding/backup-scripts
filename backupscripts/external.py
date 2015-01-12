8df2c29f52594994728c8d2269a587d7058cf415
Martin Ueding <dev@martin-ueding.de>
2015-01-12 17:55:06 +0100

    Throw out backup of info files

--- backupscripts/external.py
+++ backupscripts/external.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Copyright © 2011-2014 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2011-2015 Martin Ueding <dev@martin-ueding.de>
 
 """
 Traverses through the external drives and updates the backup on them.
@@ -87,28 +87,6 @@ def backup_data(key, name, config, dry):
     if not dry:
         backupscripts.status.update(name, 'to')
 
-def backup_info(key, name, config, dry):
-    target = config[key]['path']
-
-    termcolor.cprint("Info {}".format(name), attrs=['bold'])
-
-    if not os.path.isdir(target):
-        return
-
-    infofiles = [os.path.expanduser(path) for path in config['infofiles']['paths'].split(':')]
-
-    command = ["rsync", "-avhE", "--delete"]
-    if dry:
-        command.append('-n')
-    command += ["--"] + infofiles + [target]
-
-    try:
-        subprocess.check_call(command)
-    except subprocess.CalledProcessError as e:
-        print(e)
-    else:
-        if not dry:
-            backupscripts.status.update(name, 'to')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -122,16 +100,8 @@ def main():
     config.read(os.path.join(CONFIG_DIR, 'backup-external.ini'))
 
     targets = [key.split()[-1] for key in config.sections() if key.startswith('Target')]
-    infos = [key.split()[-1] for key in config.sections() if key.startswith('Info')]
 
-    info_pattern = re.compile(r'Info (.*)')
     data_pattern = re.compile(r'Target (.*)')
-
-
-    for key in config.sections():
-        info_matcher = info_pattern.match(key)
-        if info_matcher:
-            backup_info(key, info_matcher.group(1), config, options.dry)
 
     for key in config.sections():
         data_matcher = data_pattern.match(key)
