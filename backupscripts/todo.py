7b160a5bd190579e6de673a5c02aff210727b724
Martin Ueding <dev@martin-ueding.de>
2015-06-28 08:51:10 +0200

    More test, let them work

--- backupscripts/todo.py
+++ backupscripts/todo.py
@@ -5,8 +5,45 @@
 
 import re
 
+DONE = re.compile(r'^x (\d{4}-\d{2}-\d{2}) (.*)$')
+PRIORITY = re.compile(r'\(([A-Z])\)')
+ENTRY = re.compile(r'(\d{4}-\d{2}-\d{2})')
+
 def todo_to_taskwarrior(string):
     '''
     Takes a ``todo.txt`` like string and parses it to add to Taskwarrior.
     '''
-    
+    done, remainder = _split_done(string)
+
+    if done == False:
+        result = ['add']
+    else:
+        result = ['log', 'end:' + done]
+
+    result += [_parse_bit(bit) for bit in remainder.split()]
+
+    return result
+
+
+def _split_done(string):
+    m = DONE.search(string)
+
+    if m:
+        return m.group(1), m.group(2)
+    else:
+        return False, string
+
+
+def _parse_bit(bit):
+    m = PRIORITY.match(bit)
+    if m:
+        priority = m.group(1)
+        return 'pri:' + {'A': 'h', 'B': 'm', 'C': 'l'}[priority]
+
+    m = ENTRY.match(bit)
+    if m:
+        return 'entry:' + m.group(1)
+
+    # Nothing special could be determined about this bit, so it must be just a
+    # simple word.
+    return bit
--- tests/backupscripts/todo.py
+++ tests/backupscripts/todo.py
@@ -9,5 +9,22 @@ from backupscripts import todo
 
 class TodoTests(unittest.TestCase):
     def test_1(self):
-        r = toto.todo_to_taskwarrior('(B) 2015-06-07 Test')
-        self.assertEqual(['add', 'pri:m', 'due:2015-06-07', 'Test'])
+        r = todo.todo_to_taskwarrior('(B) 2015-06-07 Test')
+        self.assertEqual(r, ['add', 'pri:m', 'entry:2015-06-07', 'Test'])
+
+    def test_done(self):
+        r = todo.todo_to_taskwarrior('x 2015-06-20 (B) 2015-06-07 Test')
+        self.assertEqual(r, ['log', 'end:2015-06-20', 'pri:m', 'entry:2015-06-07', 'Test'])
+
+class SplitDone(unittest.TestCase):
+    def test_done(self):
+        done, remainder = todo._split_done('x 2015-06-20 (B) 2015-06-07 Test')
+
+        self.assertEqual(done, '2015-06-20')
+        self.assertEqual(remainder, '(B) 2015-06-07 Test')
+
+    def test_pending(self):
+        done, remainder = todo._split_done('(B) 2015-06-07 Test')
+
+        self.assertEqual(done, False)
+        self.assertEqual(remainder, '(B) 2015-06-07 Test')
