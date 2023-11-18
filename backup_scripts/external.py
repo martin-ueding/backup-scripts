cac16459d3154b3768b42e9a667430b2d631083d
Martin Ueding <mu@martin-ueding.de>
2023-11-18 17:44:54 +0100

    Completely rework external script

--- backup_scripts/external.py
+++ backup_scripts/external.py
@@ -1,114 +1,58 @@
-import argparse
-import configparser
-import os
-import re
 import subprocess
-
-CONFIG_DIR = os.path.expanduser("~/.config/backup-scripts")
-
-
-def read_shards(shard_names, shard_type):
-    shards = []
-    for shard in shard_names:
-        filename = os.path.join(CONFIG_DIR, shard_type, shard + ".txt")
-        with open(filename) as f:
-            shards += f.read().strip().split("\n")
-
-    shards.sort()
-
-    return shards
+import pathlib
+import tomllib
+import shlex
+import argparse
 
 
-def read_excludes(exclude_names):
-    return read_shards(exclude_names, "exclude")
+def backup_data(
+    destination: pathlib.Path,
+    includes: list[str],
+    excludes: list[str],
+    rsync_options: list[str],
+) -> None:
+    command = ["rsync", "-avhER", "--delete", "--delete-excluded"] + rsync_options
+    command += (
+        [f"--exclude={exclude}" for exclude in excludes]
+        + ["--"]
+        + includes
+        + [str(destination)]
+    )
 
-
-def read_includes(include_names):
-    return read_shards(include_names, "include")
-
-
-def backup_data(key, name, config, dry):
-    """
-    Creates a backup to ``target``.
-    """
-    excludes = read_excludes(config[key]["exclude"].split())
-    exclude_arg = ["--exclude=" + exclude for exclude in excludes]
-    sources = read_includes(config[key]["include"].split())
-
-    print("Backup {}".format(name))
-
-    abs_dest_path = config[key]["path"]
-    if "host" in config[key]:
-        dest = config[key]["host"] + ":" + abs_dest_path
-    else:
-        dest = abs_dest_path
-        if not os.path.isdir(dest):
-            print("This directory does not exist. Trying to create it …")
-            try:
-                os.makedirs(dest, exist_ok=True)
-            except PermissionError as e:
-                print(str(e))
-                return
-
-    command = ["rsync", "-avhER", "--delete", "--delete-excluded"]
-    if dry:
-        command.append("-n")
-    if "rsync-options" in config[key]:
-        command += config[key]["rsync-options"].split()
-    if "only-formats" in config[key]:
-        command.append("--include=*/")
-        for suffix in config[key]["only-formats"].split():
-            command.append("--include=*.{}".format(suffix.lower()))
-            command.append("--include=*.{}".format(suffix.upper()))
-        command.append("--exclude=*")
-    command += exclude_arg + ["--"] + sources + [dest]
-
-    print(command)
-
-    try:
-        subprocess.check_call(command)
-    except subprocess.CalledProcessError as e:
-        print(e)
-        return
-    else:
-        if not dry:
-            backup_scripts.status.update(name, "to")
-
-
-def compute_total_size(key, config):
-    sources = read_includes(config[key]["include"].split())
-    exclude_args = [
-        "--exclude-from=" + os.path.join(CONFIG_DIR, "exclude", f + ".txt")
-        for f in config[key]["exclude"].split()
-    ]
-    subprocess.check_call(["du", "-csh"] + exclude_args + sources)
+    print(shlex.join(command))
+    subprocess.run(command, check=True)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("names", nargs="*")
     parser.add_argument("-n", dest="dry", action="store_true")
-    parser.add_argument("--size", dest="size", action="store_true")
     options = parser.parse_args()
 
-    os.chdir(os.path.expanduser("~"))
+    with open(
+        pathlib.Path("~/.config/backup-scripts/backup-external.toml").expanduser(), "rb"
+    ) as f:
+        config = tomllib.load(f)
 
-    config = configparser.ConfigParser()
-    config.read(os.path.join(CONFIG_DIR, "backup-external.ini"))
+    for target_name, target_spec in config["target"].items():
+        excludes = [
+            path
+            for exclude_name in target_spec["exclude"]
+            for path in config["exclude"][exclude_name]
+        ]
+        includes = [
+            path
+            for include_name in target_spec["include"]
+            for path in config["include"][include_name]
+        ]
 
-    targets = [key.split()[-1] for key in config.sections() if key.startswith("Target")]
-
-    data_pattern = re.compile(r"Target (.*)")
-
-    for key in config.sections():
-        data_matcher = data_pattern.match(key)
-        if data_matcher:
-            name = data_matcher.group(1)
-            if len(options.names) == 0 or name in options.names:
-                if options.size:
-                    compute_total_size(key, config)
-                else:
-                    backup_data(key, name, config, options.dry)
+        destination = pathlib.Path(target_spec["path"])
+        rsync_options = target_spec.get("rsync-options", [])
+        if options.dry:
+            rsync_options.append("-n")
+        if destination.exists():
+            print(f"Creating backup of {target_name} …")
+            backup_data(destination, includes, excludes, rsync_options)
+            print(f"Done with backup of {target_name}.")
 
 
 if __name__ == "__main__":
